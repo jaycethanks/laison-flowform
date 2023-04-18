57dee7e0a7bb1baa4e0fb0c57611ce7671e1bf11
jayce <jaycethanks@outlook.com>
2023-04-18 17:57:08 +0800

    chore: 封装axios

--- playground/main.js
+++ playground/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 // import vueBus from '@/utils/vueBus';
+import { VueAxios } from '@/utils/request';
 import store from '@/store';
 import router from '@/router';
 import '@/lib/lazy_antd';
@@ -9,7 +10,7 @@ import i18n from '@/lang/index.js';
 import '@/utils/localStorageEnhance.js'; //LocalStorage 方法增强
 Vue.config.productionTip = false;
 // Vue.use(vueBus);
-
+Vue.use(VueAxios);
 new Vue({
   i18n,
   store,
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,94 @@
-import Vue from 'vue';
 import axios from 'axios';
-// import store from '@/store'
-import { VueAxios } from './axios';
-import { Modal, notification } from 'ant-design-vue';
 
-export { axios as axios };
+const baseUrl = process.env.VUE_APP_BASE_URL;
+// 创建 axios 实例
+const service = axios.create({
+  //baseURL: '/jeecg-boot',
+  baseURL: baseUrl, // api base_url
+  timeout: 30000, // 请求超时时间
+});
+
+// request interceptor
+service.interceptors.request.use(
+  function (config) {
+    if (localStorage.getItem('token')) {
+      config.headers.token = localStorage.getItem('token');
+      return config;
+    } else {
+      return config;
+    }
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+// response interceptor
+service.interceptors.response.use(
+  (response) => {
+    // console.log('[response]: ', response);
+    // if (process.env.NODE_ENV === 'mock') {
+    //   return injectMock();
+    // }
+    // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
+    // 否则的话抛出错误
+    if (response.data.errorcode === 401) {
+      window.location.href = 'login.html';
+    } else if (response.data.errorcode === 403) {
+      Message({
+        message: 'The login information has expired. Please login again.',
+        type: 'error',
+        duration: 5 * 1000,
+      });
+      localStorage.removeItem('token');
+      window.location.href = 'login.html';
+    } else {
+      return response.data;
+    }
+  },
+  // 服务器状态码不是2开头的的情况
+  // 然后根据返回的状态码进行一些操作，例如登录过期提示，错误提示等等
+  (error) => {
+    Message({
+      message: error.response.statusText,
+      type: 'error',
+      duration: 5 * 1000,
+    });
+  },
+);
+
+const VueAxios = {
+  vm: {},
+  // eslint-disable-next-line no-unused-vars
+  install(Vue, router = {}, instance) {
+    if (this.installed) {
+      return;
+    }
+    this.installed = true;
+
+    if (!instance) {
+      // eslint-disable-next-line no-console
+      console.error('You have to install axios');
+      return;
+    }
+
+    Vue.axios = instance;
+
+    Object.defineProperties(Vue.prototype, {
+      $axios: {
+        get: function get() {
+          return instance;
+        },
+      },
+    });
+  },
+};
+
+const installer = {
+  vm: {},
+  install(Vue, router = {}) {
+    Vue.use(VueAxios, router, service);
+  },
+};
+
+export { installer as VueAxios, service as axios };
