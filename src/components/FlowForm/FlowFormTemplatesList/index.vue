1e345a423080328eae5367946fc840d189f3ac5b
jayce <jaycethanks@outlook.com>
2023-06-01 17:16:50 +0800

    feat: 系统模板管理页面优化

--- src/components/FlowForm/FlowFormTemplatesList/index.vue
+++ src/components/FlowForm/FlowFormTemplatesList/index.vue
@@ -1,91 +1,84 @@
 <template>
-  <div class="process-list-root" ref="process-list-root" :style="{ height: 'calc(100% - 32px)' }">
-    <section class="collapse-wrapper" v-if="list.length > 0">
-      <div class="collapse-content">
-        <a-collapse :bordered="false" v-model="activeKey">
-          <a-collapse-panel v-for="group in list" :key="group.groupId" :style="customStyle">
-            <!-- group 设置 -->
-            <a-dropdown slot="extra" v-if="false">
-              <a-button type="link">
-                <a-icon type="setting" />
-              </a-button>
-              <a-menu slot="overlay">
-                <a-menu-item>
-                  <a-button
-                    size="small"
-                    v-show="group.notEditable"
-                    icon="edit"
-                    type="link"
-                    @click.prevent.stop="group.notEditable = false"
-                    >重命名</a-button
-                  >
-                </a-menu-item>
-              </a-menu>
-            </a-dropdown>
-            <template slot="header">
-              <a-space>
-                <a-input
-                  size="small"
-                  :style="{ border: group.notEditable ? 'none' : '', useSelect: 'none' }"
-                  :disabled="group.notEditable"
-                  @click.prevent.stop
-                  @change.stop.prevent
-                  v-model="group.groupName"
-                />
-
-                <a-button
-                  v-show="!group.notEditable"
-                  icon="check"
-                  type="link"
-                  :loading="ifloadingCheck"
-                  @click.prevent.stop="handleGroupNameEdit(group)"
-                ></a-button>
-              </a-space>
-            </template>
-            <a-table :columns="columns" :data-source="group.list" rowKey="id" size="small" :pagination="false">
-              <template slot="id" slot-scope="text, record">
-                <div class="icon-box" :style="{ backgroundColor: record.designColor }">
-                  <img :src="getIcon(record.designIcon)" alt="" />
-                </div>
-              </template>
-              <a slot="enable" slot-scope="text, record">
-                <a-switch
-                  :loading="loadingSwitch"
-                  @change="(e) => onSwitchChange(e, record.id)"
-                  :checked="record.enable"
-                  size="small"
-                />
-              </a>
-              <a slot="action" slot-scope="text, record">
-                <a-space>
-                  <a-button type="link" icon="edit" size="small" @click="handleEdit(record)">编辑</a-button>
-                  <a-popconfirm
-                    placement="rightBottom"
-                    ok-text="Yes"
-                    cancel-text="No"
-                    @confirm="handleDelete(record.id)"
-                  >
-                    <template slot="title"> 确定删除？ </template>
-                    <a-button style="color: #ff4d4f" type="link" icon="delete" size="small">删除</a-button>
-                  </a-popconfirm>
-                </a-space>
-              </a>
-            </a-table>
-          </a-collapse-panel>
-        </a-collapse>
-      </div>
-    </section>
-    <EmptyPage v-else />
-  </div>
+  <Container>
+    <a-table
+      size="small"
+      :columns="group_columns"
+      :expandRowByClick="true"
+      :data-source="list"
+      rowKey="groupId"
+      :pagination="false"
+    >
+      <template slot="groupName" slot-scope="text, group">
+        <a-space>
+          <a-input
+            size="small"
+            :style="{ border: group.notEditable ? 'none' : '', useSelect: 'none' }"
+            :disabled="group.notEditable"
+            @click.prevent.stop
+            @change.stop.prevent
+            v-model="group.groupName"
+          />
+          <a-button
+            v-show="!group.notEditable"
+            icon="check"
+            type="link"
+            :loading="group.ifloadingCheck"
+            @click.prevent.stop="handleGroupNameEdit(group)"
+          ></a-button>
+        </a-space>
+      </template>
+      <!-- <a slot="action" slot-scope="text, group"> -->
+      <a-space v-if="false" slot="action" slot-scope="text, group">
+        <a-button
+          size="small"
+          v-show="group.notEditable"
+          icon="edit"
+          type="link"
+          @click.prevent.stop="group.notEditable = false"
+          >重命名</a-button
+        >
+      </a-space>
+      <!-- </a> -->
+      <p slot="expandedRowRender" slot-scope="record" style="margin: 0">
+        <a-table :columns="columns" :data-source="record.list" rowKey="id" size="small" :pagination="false">
+          <template slot="id" slot-scope="text, record">
+            <ffIcon size="small" :bgc="record.designColor" :icon="record.designIcon" />
+            <!-- <div class="icon-box" :style="{ backgroundColor: record.designColor }">
+              <img :src="getIcon(record.designIcon)" alt="" />
+            </div> -->
+          </template>
+          <a slot="enable" slot-scope="text, record">
+            <a-switch
+              :loading="loadingSwitch"
+              @change="(e) => onSwitchChange(e, record.id)"
+              :checked="record.enable"
+              size="small"
+            />
+          </a>
+          <a slot="action" slot-scope="text, record">
+            <a-space>
+              <a-button type="link" icon="edit" size="small" @click="handleEdit(record)">编辑</a-button>
+              <a-popconfirm placement="rightBottom" ok-text="Yes" cancel-text="No" @confirm="handleDelete(record.id)">
+                <template slot="title"> 确定删除？ </template>
+                <a-button style="color: #ff4d4f" type="link" icon="delete" size="small">删除</a-button>
+              </a-popconfirm>
+            </a-space>
+          </a>
+        </a-table>
+      </p>
+    </a-table>
+  </Container>
 </template>
 <script>
 import API from '@/api/ErpConfig.js';
 import EmptyPage from '@/components/FlowForm/EmptyPage/index.vue';
 import { listPlatformGroup, update, deleteById, listDesignGroup } from '@/api/system/ffTemplate.js';
 import icons from '@/assets/flowform_icons/index.js';
+import ffIcon from "@/components/FlowForm/ffIcon/index.vue"
 import mock from './mock';
 import ellipsis from '@/utils/ellipsis';
 import FlowFormDesignerType from "@/constants/FlowFormDesignerType.js"
+import Container  from "@/components/base/Container/index.vue"
 const columns = [
   {
     title: '',
@@ -99,6 +92,7 @@ const columns = [
     title: '模板名称',
     dataIndex: 'designName',
     key: 'designName',
+
     width: 200,
   },
   {
@@ -123,14 +117,39 @@ const columns = [
     width: 150,
   },
 ];
+const group_columns = [
+  // {
+  //   title: '',
+  //   dataIndex: 'id',
+  //   key: 'id',
+  //   width: 180,
+  // },
+  {
+    title: '分组名称',
+    dataIndex: 'groupName',
+    key: 'groupName',
+    scopedSlots: { customRender: 'groupName' },
+
+    // width: 200,
+  },
+    {
+    title: '操作',
+    dataIndex: 'action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+    width: 150,
+  },
+]
 
 export default {
-  components: { EmptyPage },
+  name:"FlowFormTemplatesList",
+  components: { EmptyPage,ffIcon,Container },
   data() {
     return {
-      activeKey: [],
-      ifloadingCheck: false,
+
+      expandedRowKeys:[],
       columns,
+      group_columns,
       list: [],
       icons,
       loadingSwitch: false,
@@ -172,16 +191,13 @@ export default {
     },
 
     async loadList() {
-      // TODO：  返回的数据中要求新增 groupId 字段
       const res = await listPlatformGroup();
       if (res.status === 200) {
         res.data.forEach((item) => {
           // !!notEditable & selectedRowKeys 仅前端运行时有效，提交后台之前，数据中的这两个字段应该移除
           item.notEditable = true;
-          // item.selectedRowKeys = [];
-          item.groupId = Math.random().toString(36).substr(2);
+          item.ifloadingCheck = false
 
-          this.activeKey.push(item.groupId);
         });
         this.list = res.data;
       }
@@ -189,10 +205,10 @@ export default {
     handleGroupNameEdit(group) {
       // TODO：  后端需要新增接口 handle groupName 分组名称的修改
       const { groupName, groupId } = group;
-      this.ifloadingCheck = true;
+      group.ifloadingCheck = true,
       setTimeout(() => {
         group.notEditable = true;
-        this.ifloadingCheck = false;
+        group.ifloadingCheck = false;
       }, 2000);
       console.log(groupName, groupId);
     },
@@ -246,9 +262,9 @@ export default {
 //   overflow-y: auto;
 // }
 
-::v-deep .ant-collapse-header {
-  padding: 0;
-}
+// ::v-deep .ant-collapse-header {
+//   padding: 0;
+// }
 ::v-deep .ant-table-small{
 border: none;
 }
@@ -263,65 +279,65 @@ border: none;
   -ms-user-select: none; /* IE 10+ */
   user-select: none;
 }
-.process-list-root {
-  padding-top: 10px;
-  display: flex;
-  justify-content: center;
-  position: relative;
-  user-select: none;
-  // overflow-y: scroll;
-  // .head-operation-wrapper {
-  //   overflow-y: scroll;
-  //   width: 30%;
-  //   min-width: 300px;
-  //   flex: 1;
-  //   padding: 4px 12px;
-  //   position: relative;
-  // }
-  .collapse-wrapper {
-    height: 100%;
-    overflow-y: scroll;
-    .collapse-content {
-      // border: 1px solid red;
-      // overflow-y: scroll;
-    }
+// .process-list-root {
+//   padding-top: 10px;
+//   display: flex;
+//   justify-content: center;
+//   position: relative;
+//   user-select: none;
+//   // overflow-y: scroll;
+//   // .head-operation-wrapper {
+//   //   overflow-y: scroll;
+//   //   width: 30%;
+//   //   min-width: 300px;
+//   //   flex: 1;
+//   //   padding: 4px 12px;
+//   //   position: relative;
+//   // }
+//   .collapse-wrapper {
+//     height: 100%;
+//     overflow-y: scroll;
+//     .collapse-content {
+//       // border: 1px solid red;
+//       // overflow-y: scroll;
+//     }
 
-    flex: 3;
-    // border: 1px solid red !important;
-  }
+//     flex: 3;
+//     // border: 1px solid red !important;
+//   }
 
-  .a-drawer-wapper {
-  }
-  .show-details-wrap {
-    font-size: 14px;
-    cursor: pointer;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    height: 30px;
-    width: 100%;
-    background-color: #f8f8f8;
-    display: flex;
-    gap: 20px;
-    justify-content: center;
-    align-items: center;
-  }
-  &::-webkit-scrollbar {
-    width: 2px;
-    height: 6px;
-  }
+//   .a-drawer-wapper {
+//   }
+//   .show-details-wrap {
+//     font-size: 14px;
+//     cursor: pointer;
+//     position: absolute;
+//     bottom: 0;
+//     left: 0;
+//     height: 30px;
+//     width: 100%;
+//     background-color: #f8f8f8;
+//     display: flex;
+//     gap: 20px;
+//     justify-content: center;
+//     align-items: center;
+//   }
+//   &::-webkit-scrollbar {
+//     width: 2px;
+//     height: 6px;
+//   }
 
-  .icon-box {
-    width: 36px;
-    height: 36px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 5px;
-    position: relative;
-    img {
-      width: 24px;
-    }
-  }
-}
+//   .icon-box {
+//     width: 36px;
+//     height: 36px;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     border-radius: 5px;
+//     position: relative;
+//     img {
+//       width: 24px;
+//     }
+//   }
+// }
 </style>
