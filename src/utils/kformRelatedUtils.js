a3396d38c89ae905821f5b26961a8b92b01f5f24
jayce <jaycethanks@outlook.com>
2023-06-05 10:41:29 +0800

    feat: 表单解析函数重写

--- src/utils/kformRelatedUtils.js
+++ src/utils/kformRelatedUtils.js
@@ -3,33 +3,65 @@
  * @param {*} mock
  * @returns
  */
-export const parseFormWithNodeConfig = function (formWithNodeConfig) {
-  // todo: 实际的表单可能是一个复杂树结构， 应该递归处理
-  const { formInfo, nodeConfigs } = formWithNodeConfig;
-  let jsonFormInfo = typeof formInfo === 'string' ? JSON.parse(formInfo) : formInfo;
-  console.log('[jsonFormInfo]: ', jsonFormInfo);
+function walkNodes(list, callback) {
+  // 递归遍历控件树
+  const traverse = (array) => {
+    array.forEach((element) => {
+      callback(element);
+      // this.$set(element.options, optionName, value);
 
-  // 解析失败直接返回null, 上层handle需要添加处理逻辑提示页面
-  return {
-    list: [],
-    config: {
-      layout: 'horizontal',
-      labelCol: { xs: 4, sm: 4, md: 4, lg: 4, xl: 4, xxl: 4 },
-      labelWidth: 100,
-      labelLayout: 'flex',
-      wrapperCol: { xs: 18, sm: 18, md: 18, lg: 18, xl: 18, xxl: 18 },
-      hideRequiredMark: false,
-      customStyle: '',
-      enablePrint: false, //@jayce
-      Expressions: '', //@jayce
-      currentLang: 'zh', //@jayce 23/05/10-09:22:11 :
-      supportedLanguages: ['zh'], //@jayce 23/05/10-10:39:41 :
-    },
-    //@jayce 23/05/10-10:39:50 :
-    predefinedLists: {
-      zh: [],
-    },
+      if (element.type === 'grid' || element.type === 'tabs') {
+        // 栅格布局 and 标签页
+        element.columns.forEach((item) => {
+          traverse(item.list);
+        });
+      } else if (element.type === 'card' || element.type === 'batch') {
+        // 卡片布局 and  动态表格
+        traverse(element.list);
+      } else if (element.type === 'table') {
+        // 表格布局
+        element.trs.forEach((item) => {
+          item.tds.forEach((val) => {
+            traverse(val.list);
+          });
+        });
+      }
+    });
   };
+  traverse(list);
+}
+
+export const parseFormWithNodeConfig = function (formWithNodeConfig, currentLang = 'zh') {
+  try {
+    const { formInfo, formConfigs } = formWithNodeConfig;
+    const jsonFormInfo = typeof formInfo === 'string' ? JSON.parse(formInfo) : formInfo;
+    if (!jsonFormInfo) {
+      return null;
+    }
+    // 取出配置， set到对应的语言 list
+    const list = jsonFormInfo.predefinedLists[currentLang];
+    walkNodes(list, (element) => {
+      const findConfig = formConfigs.find((config) => config.key === element.key);
+      if (findConfig != undefined) {
+        const { disabled, hidden } = findConfig;
+        element.options['hidden'] = hidden;
+        element.options['disabled'] = disabled;
+        if (typeof findConfig.exposeFields !== 'undefined') {
+          // 如果这个字段是自定义组件， 就会可能会存在 exposeFields 这个字段 (取决与自定义组件中 是否有按照规定实现)
+          // 将exposeFields 写入到 formInfo 在组件解析阶段去控制自定义组件中的对应字段
+          element._exposeFields = findConfig.exposeFields;
+        }
+      }
+    });
+
+    // 将当前表单 list 设定为已配置的目标语言 list
+    jsonFormInfo.list = list;
+    return jsonFormInfo;
+  } catch (e) {
+    console.error('表单解析异常:\n', e);
+    return null;
+  }
+
   // 渲染一个表单， 需要 当前结点配置的类型： 审批/抄送/查看 + 当前结点的 id
   // mock.currentNodeType = 'taskConfig'; // 后端返回的时候，需要标记当前结点的类型
   // mock.currentNodeId = 'Activity_12sck8q';
