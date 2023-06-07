6ed06e65d36f30ef3e8cdfb79739cc0630ddab30
jayce <jaycethanks@outlook.com>
2023-06-07 16:47:47 +0800

    style: 设定统一主题变量

---
+++ src/components/base/baseStyle.js
@@ -0,0 +1,53 @@
+export default {
+  // bg
+  $primary: {
+    bg: '#0d6efd',
+    bg_light: '#cfe2ff',
+    text: '#052c65',
+    border: '#9ec5fe',
+  },
+  $secondary: {
+    bg: '#6c757d',
+    bg_light: '#e2e3e5',
+    text: '#2b2f32',
+    border: '#c4c8cb',
+  },
+  $success: {
+    bg: '#198754',
+    bg_light: '#d1e7dd',
+    text: '#0a3622',
+    border: '#a3cfbb',
+  },
+  $info: {
+    bg: '#0dcaf0',
+    bg_light: '#cff4fc',
+    text: '#055160',
+    border: '#9eeaf9',
+  },
+  $warning: {
+    bg: '#ffc107',
+    bg_light: '#fff3cd',
+    text: '#664d03',
+    border: '#ffe69c',
+  },
+  $danger: {
+    bg: '#dc3545',
+    bg_light: '#fff3cd',
+    text: '#58151c',
+    border: '#f1aeb5',
+  },
+  $light: {
+    bg: '#f8f9fa',
+    bg_light: '#fcfcfd',
+    text: '#495057',
+    border: '#e9ecef',
+  },
+  $dark: {
+    bg: '#212529',
+    bg_light: '#ced4da',
+    text: '#495057',
+    border: '#adb5bd',
+  },
+};
+
+// Bootstrap 主题色
