4978bf6d1b65adaafd05cd9e2a6d1fadfe7ec124
jayce <jaycethanks@outlook.com>
2023-06-15 17:35:05 +0800

    feat: TimeLine 撤回颜色修改

--- src/packages/FormPreviewer/TimeLine.vue
+++ src/packages/FormPreviewer/TimeLine.vue
@@ -78,7 +78,7 @@ const _fileUrl = process.env.VUE_APP_FILE_URL;
 const colors = {
   create: baseStyle.$primary,
   pass: baseStyle.$success,
-  cancel: baseStyle.$warning,
+  cancel: baseStyle.$danger,
   back: baseStyle.$danger,
   delegate: baseStyle.$info,
   change: baseStyle.$dark,
